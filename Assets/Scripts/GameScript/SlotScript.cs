cd7cd0ebbddb3b954d68830e7a91b1f7dcf40371
CharlieYu99 <yujicen99@126.com>
2022-06-26 15:16:49 +0100

    slot script adjusted, slot prefab added

--- Assets/Scripts/GameScript/SlotScript.cs
+++ Assets/Scripts/GameScript/SlotScript.cs
@@ -9,6 +9,13 @@ public class SlotScript : Scaffold.Thing
     public Image slotIcon;
     public bool isPeek;
 
+    public Sprite fireballIcon;
+    public Sprite splitFireballIcon;
+    public Sprite meteoroliteIcon;
+    public Sprite defendIcon;
+    public Sprite movementIcon;
+    public Sprite peekIcon;
+
     private void Start()
     {
         isFilled = false;
@@ -23,6 +30,34 @@ public class SlotScript : Scaffold.Thing
     {
         slotIcon.sprite = sprite;
         isFilled = true;
+    }
+
+    public void SetIcon(VerbDef verbDef)
+    {
+        if (verbDef == VerbDefOf.attackFireball)
+        {
+            SetIcon(fireballIcon);
+        }
+        if (verbDef == VerbDefOf.attackSplitFireball)
+        {
+            SetIcon(splitFireballIcon);
+        }
+        if (verbDef == VerbDefOf.attackMeteorolite)
+        {
+            SetIcon(meteoroliteIcon);
+        }
+        if (verbDef == VerbDefOf.defendBasic)
+        {
+            SetIcon(defendIcon);
+        }
+        if (verbDef == VerbDefOf.movementBasic)
+        {
+            SetIcon(movementIcon);
+        }
+        if (verbDef == VerbDefOf.peek)
+        {
+            SetIcon(peekIcon);
+        }
     }
 
     public void RemoveIcon()
